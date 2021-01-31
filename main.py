5c5da528047f28ff8339f621c9f319d7ea33326e
Will Holland <snowy.the.crow@gmail.com>
2021-01-31 11:16:31 +0700

    use a seed so random samples are reproducible
    if no seed is given, it generates a random one and redirects

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import uuid
+import urllib
 import random
-from bottle import route, run, template, request, abort
+from bottle import route, run, template, request, abort, redirect
 
 
 LEVELS = os.listdir('words')
@@ -11,8 +12,18 @@ def make_id():
     return uuid.uuid4().int
 
 
-def get_word_list():
-    ''' this will return the webpage which is a list of words '''
+def get_seeded_sample(words, seed, quantity):
+    '''
+        given words (list of strings), a seed (int) and a quantity (int),
+        return a list of size `quantity` from the `words` that will always
+        return the same given the same seed
+    '''
+    random.seed(seed)
+    return random.sample(words, quantity)
+
+
+def get_word_list_from_seed(seed):
+    ''' this will return the webpage which is a list of words from a seed'''
     quantity = request.query.quantity
     if quantity:
         quantity = int(quantity)
@@ -24,7 +35,7 @@ def get_word_list():
     the_file = os.path.join('words', level)
     with open(the_file) as f:
         words = [line for line in f]
-    words = sorted(random.sample(words, int(quantity)))
+    words = get_seeded_sample(words, seed, quantity)
     return template('list', res=words)
 
 
@@ -36,9 +47,14 @@ def get_homepage_form():
 @route('/words/')
 def index():
     ''' entry point for all requests '''
-    if request.query.level:
-        # if level is passed then we want a list of words
-        return get_word_list()
+    if request.query.seed:
+        seed = request.query.seed
+        return get_word_list_from_seed(seed)
+    elif request.query.level and request.query.quantity:
+        params = {k: v[0] for k, v in request.query.dict.items()}
+        params['seed'] = make_id()
+        params = urllib.parse.urlencode(params)
+        redirect('.?' + params)
     else:
         # otherwise we go to the landing page
         return template('index', levels=LEVELS)
